6edbcf625d5ae3600eccf8e0e2f17d8de4702f12
cedric-coder <cedricmassat1@gmail.com>
2021-11-19 13:11:46 +0100

    added color on VisualDebugger

--- src/game/VisualDebugger.java
+++ src/game/VisualDebugger.java
@@ -1,7 +1,7 @@
 package game;
 
 import game.GameUtility.GameState;
-import game.character.Ghosts.Ghost;
+import game.character.Ghosts.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,9 +9,22 @@ import java.awt.*;
 public class VisualDebugger {
     public static void draw(JPanel jPanel, Graphics graphics) {
         for (Ghost ghost:PacManGame.ghosts) {
-           Engines.drawImage(jPanel,(Graphics2D) graphics,
-                   new ImageIcon("src/game/resources/croix.png").getImage(),
+            if(ghost instanceof Blinky)
+                Engines.drawImage(jPanel,(Graphics2D) graphics,
+                   new ImageIcon("src/game/resources/croixRouge.png").getImage(),
                    ghost.target.x,ghost.target.y,PacManGame.gameUnit);
+            if(ghost instanceof Pinky)
+                Engines.drawImage(jPanel,(Graphics2D) graphics,
+                        new ImageIcon("src/game/resources/croixRose.png").getImage(),
+                        ghost.target.x,ghost.target.y,PacManGame.gameUnit);
+            if(ghost instanceof Inky)
+                Engines.drawImage(jPanel,(Graphics2D) graphics,
+                        new ImageIcon("src/game/resources/croixBleu.png").getImage(),
+                        ghost.target.x,ghost.target.y,PacManGame.gameUnit);
+            if(ghost instanceof Clyde)
+                Engines.drawImage(jPanel,(Graphics2D) graphics,
+                        new ImageIcon("src/game/resources/croixOrange.png").getImage(),
+                        ghost.target.x,ghost.target.y,PacManGame.gameUnit);
         }
 
     }
