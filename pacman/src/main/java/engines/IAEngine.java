7145e003c88aca8e1986f2716c0c243957a5d25b
cedric-coder <cedricmassat1@gmail.com>
2021-12-01 14:22:26 +0100

    renamed var

--- pacman/src/main/java/engines/IAEngine.java
+++ pacman/src/main/java/engines/IAEngine.java
@@ -5,26 +5,25 @@ import utility.GameObject;
 import utility.Vector2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class IAEngine {
-    protected static Direction getDirReducingDist(GameObject from, Vector2 to, List<Direction> directions, int step){
+    protected static Direction getDirReducingDist(GameObject gameObject, Vector2 to, List<Direction> directions, int step){
         List<Double> listOfDist = new ArrayList<>();
         int indexOfBest = 0;
         for (int i = 0; i < directions.size(); i++) {
-            listOfDist.add(CoreKernel.calculateDist(Vector2.add(from.getPosition(),CoreKernel.getVectorFromDir(directions.get(i),step)),to));
+            listOfDist.add(CoreKernel.calculateDist(Vector2.add(gameObject.getPosition(),CoreKernel.getVectorFromDir(directions.get(i),step)),to));
             if(listOfDist.get(i) < listOfDist.get(indexOfBest))
                 indexOfBest = i;
         }
         return directions.get(indexOfBest);
     }
 
-    protected static Direction getDirIncreasingDist(GameObject from, Vector2 to, List<Direction> directions, int step){
+    protected static Direction getDirIncreasingDist(GameObject gameObject, Vector2 to, List<Direction> directions, int step){
         List<Double> listOfDist = new ArrayList<>();
         int indexOfBest = 0;
         for (int i = 0; i < directions.size(); i++) {
-            listOfDist.add(CoreKernel.calculateDist(Vector2.add(from.getPosition(),CoreKernel.getVectorFromDir(directions.get(i),step)),to));
+            listOfDist.add(CoreKernel.calculateDist(Vector2.add(gameObject.getPosition(),CoreKernel.getVectorFromDir(directions.get(i),step)),to));
             if(listOfDist.get(i) > listOfDist.get(indexOfBest))
                 indexOfBest = i;
         }
